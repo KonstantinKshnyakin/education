73f93c439990fee02d751ce645e9b48884e56d9d
K.Kshnyakin <kocmoc007@gmail.com>
2020-10-28 19:39:12 +0300

    rebase spring-mvc-app-alishev

--- src/main/java/ru/alishev/springcourse/controllers/FirstController.java
+++ src/main/java/ru/alishev/springcourse/controllers/FirstController.java
@@ -1,11 +1,10 @@
 package ru.alishev.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
@@ -13,13 +12,32 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
-        System.out.println("Hello, " + name + " " + surname);
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+//        System.out.println("Hello, " + name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
     public String goodByePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String getCalculate(@RequestParam("a") int a,
+                               @RequestParam("b") int b,
+                               @RequestParam("action") String action,
+                               Model model) {
+
+        double result = switch (action) {
+            case "multiplication" -> a * b;
+            case "addition" -> a + b;
+            case "subtraction" -> a - b;
+            case "division" -> a / (double) b;
+            default -> 0;
+        };
+        model.addAttribute("result", result);
+        return "first/calculator";
     }
 }
