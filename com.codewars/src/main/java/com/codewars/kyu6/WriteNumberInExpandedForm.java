62f664f5c83a0d83644b9b9f9ecf02529ec0160f
K.Kshnyakin <kocmoc007@gmail.com>
2020-11-11 17:22:26 +0300

    Write Number in Expanded Form

---
+++ com.codewars/src/main/java/com/codewars/kyu6/WriteNumberInExpandedForm.java
@@ -0,0 +1,45 @@
+package com.codewars.kyu6;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * You will be given a number and you will need to return it as
+ * a string in Expanded Form. For example:
+ * <p>
+ * Kata.expandedForm(12); # Should return "10 + 2"
+ * Kata.expandedForm(42); # Should return "40 + 2"
+ * Kata.expandedForm(70304); # Should return "70000 + 300 + 4"
+ * <p>
+ * NOTE: All numbers will be whole numbers greater than 0.
+ */
+public class WriteNumberInExpandedForm {
+
+    public static String expandedForm(int num) {
+        StringBuilder builder = new StringBuilder();
+        int tensMultiplier = 1;
+        while (num > 0) {
+            int i = num % 10;
+            num = num / 10;
+            if (i != 0) {
+                builder.insert(0, " + ");
+                builder.insert(0, i * tensMultiplier);
+            }
+            if (num == 0) {
+                int plusPosition = builder.lastIndexOf("+");
+                builder.delete(plusPosition - 1, plusPosition + 2);
+            }
+            tensMultiplier *= 10;
+        }
+        return builder.toString();
+    }
+
+    @Test
+    public void testSomething() {
+        assertEquals("10 + 2", expandedForm(12));
+        assertEquals("40 + 2", expandedForm(42));
+        assertEquals("70000 + 300 + 4", expandedForm(70304));
+        assertEquals("9000000", expandedForm(9000000));
+    }
+}
