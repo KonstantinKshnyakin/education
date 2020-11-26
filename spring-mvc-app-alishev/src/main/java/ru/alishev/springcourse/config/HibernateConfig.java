27ef7b53e42ca7d1c842a8a93c7fd2576f695601
K.Kshnyakin <kocmoc007@gmail.com>
2020-11-26 14:16:45 +0300

    rebase spring-mvc-app-alishev

--- spring-mvc-app-alishev/src/main/java/ru/alishev/springcourse/config/HibernateConfig.java
+++ spring-mvc-app-alishev/src/main/java/ru/alishev/springcourse/config/HibernateConfig.java
@@ -1,7 +1,6 @@
 package ru.alishev.springcourse.config;
 
 import org.apache.commons.dbcp2.BasicDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
 import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
