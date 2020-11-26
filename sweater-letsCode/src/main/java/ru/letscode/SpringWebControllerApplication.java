89fed1913d2c95dd0590d69d8dec98f64e3ebb88
K.Kshnyakin <kocmoc007@gmail.com>
2020-11-26 19:31:59 +0300

    Spring Boot: делаем простое веб приложение на Java (простой сайт)

---
+++ sweater-letsCode/src/main/java/ru/letscode/SpringWebControllerApplication.java
@@ -0,0 +1,12 @@
+package ru.letscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebControllerApplication.class);
+    }
+}
