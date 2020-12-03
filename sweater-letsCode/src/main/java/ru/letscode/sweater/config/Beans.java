b18e0ec7bb76149b427401ffa457b5b473408328
K.Kshnyakin <kocmoc007@gmail.com>
2020-12-03 20:57:13 +0300

    Spring Boot: reCaptcha, rest client, rememberMe и сохранение сессий в БД

--- sweater-letsCode/src/main/java/ru/letscode/sweater/config/Beans.java
+++ sweater-letsCode/src/main/java/ru/letscode/sweater/config/Beans.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class Beans {
@@ -11,5 +12,10 @@ public class Beans {
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder(8);
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
     }
 }
--- sweater-letsCode/src/main/java/ru/letscode/sweater/controller/RegistrationController.java
+++ sweater-letsCode/src/main/java/ru/letscode/sweater/controller/RegistrationController.java
@@ -1,24 +1,37 @@
 package ru.letscode.sweater.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 import ru.letscode.sweater.entyti.User;
+import ru.letscode.sweater.entyti.dto.CaptchaResponseDto;
 import ru.letscode.sweater.services.UserService;
 
 import javax.validation.Valid;
+import java.util.Collections;
 import java.util.Map;
 
 @Controller
 public class RegistrationController {
 
-    private final UserService userService;
+    public static final String CAPTCHA_URL = "https://www.google.com/recaptcha/api/siteverify?secret=%s&response=%s";
 
-    public RegistrationController(UserService userService) {
+    private final UserService userService;
+    private final RestTemplate restTemplate;
+
+    @Value("${recaptcha.secret}")
+    private String secret;
+
+    public RegistrationController(UserService userService, RestTemplate restTemplate) {
         this.userService = userService;
+        this.restTemplate = restTemplate;
     }
 
     @GetMapping("registration")
@@ -27,12 +40,29 @@ public class RegistrationController {
     }
 
     @PostMapping("registration")
-    public String addUser(@Valid User user, BindingResult bindingResult, Model model) {
-        if (user.getPassword() != null && !user.getPassword().equals(user.getPassword2())) {
+    public String addUser(
+            @RequestParam("g-recaptcha-response") String captchaResponse,
+            @RequestParam("password2") String passwordConfirm,
+            @Valid User user,
+            BindingResult bindingResult,
+            Model model) {
+        String url = String.format(CAPTCHA_URL, secret, captchaResponse);
+        CaptchaResponseDto response = restTemplate.postForObject(url, Collections.emptyList(), CaptchaResponseDto.class);
+
+        if (!response.isSuccess()) {
+            model.addAttribute("captchaError", "Fill captcha");
+        }
+        boolean isConfirmEmpty = StringUtils.isEmpty(passwordConfirm);
+
+        if (isConfirmEmpty) {
+            model.addAttribute("password2Error", "Password confirmation cannot be empty");
+        }
+
+        if (user.getPassword() != null && !user.getPassword().equals(passwordConfirm)) {
             model.addAttribute("passwordError", "Password are different!");
         }
 
-        if (bindingResult.hasErrors()) {
+        if (bindingResult.hasErrors() || isConfirmEmpty || !response.isSuccess()) {
             Map<String, String> errorsMap = ControllerUtils.getErrors(bindingResult);
             model.mergeAttributes(errorsMap);
             return "registration";
@@ -48,8 +78,10 @@ public class RegistrationController {
     public String activate(@PathVariable String code, Model model) {
         boolean isActivated = userService.activateUser(code);
         if (isActivated) {
+            model.addAttribute("messageType", "successes");
             model.addAttribute("message", "User successfully activated");
         } else {
+            model.addAttribute("messageType", "danger");
             model.addAttribute("message", "Activation code is not found!");
         }
         return "login";
