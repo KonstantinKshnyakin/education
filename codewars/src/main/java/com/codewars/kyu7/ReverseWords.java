fa315bdc281cd5150c049b0ccc4dc94968dcca23
K.Kshnyakin <kocmoc007@gmail.com>
2020-12-05 21:27:16 +0300

    Reverse words

---
+++ codewars/src/main/java/com/codewars/kyu7/ReverseWords.java
@@ -0,0 +1,35 @@
+package com.codewars.kyu7;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/****************************************************************************************
+ *Complete the function that accepts a string parameter, and reverses each word in
+ * the string. All spaces in the string should be retained.
+ ***************************************************************************************/
+public class ReverseWords {
+
+    public static String reverseWords(final String original) {
+        if (original.isBlank()) {
+            return original;
+        }
+        return Stream.of(original.split(" "))
+                .map(
+                        s -> new StringBuilder(s).reverse().append(" ").toString()
+                )
+                .collect(Collectors.joining())
+                .substring(0, original.length());
+    }
+
+    @Test
+    public void exampleCases() {
+        assertEquals("ehT kciuq nworb xof spmuj revo eht yzal .god", reverseWords("The quick brown fox jumps over the lazy dog."));
+        assertEquals("elppa", reverseWords("apple"));
+        assertEquals("a b c d", reverseWords("a b c d"));
+        assertEquals("elbuod  decaps  sdrow", reverseWords("double  spaced  words"));
+    }
+}
