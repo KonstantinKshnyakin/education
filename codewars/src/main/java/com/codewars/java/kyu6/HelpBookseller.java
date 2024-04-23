408c533f5d4d39f3536901741143bcb29dbe7a5e
Константин <kocmoc007@gmail.com>
2024-04-23 16:07:25 +0300

    GoodVsEvil

--- codewars/src/main/java/com/codewars/java/kyu6/HelpBookseller.java
+++ codewars/src/main/java/com/codewars/java/kyu6/HelpBookseller.java
@@ -2,7 +2,6 @@ package com.codewars.java.kyu6;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.function.Function;
